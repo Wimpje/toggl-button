71977e5179aa114f2ea7c70339ed56f74dc4aeb9
Wim <wimhoringa@gmail.com>
2014-07-09 16:59:14 +0200

    Added support for mango

---
+++ src/scripts/content/fogbugz.js
@@ -0,0 +1,23 @@
+/*jslint indent: 2, unparam: true*/
+/*global $: false, document: false, togglbutton: false*/
+'use strict';
+
+togglbutton.render('#bugviewContainer', {}, function (elem) {
+  var link, description,
+    numElem = $('#bugviewContainer .ixBug a'),
+    titleElem = $('.idTitleProjectAndArea .title'),
+    projectElem = $('.idTitleProjectAndArea .subtitle a');
+
+  description = titleElem.innerText;
+  if (numElem !== null) {
+    description = numElem.innerText + " " + description;
+  }
+
+  link = togglbutton.createTimerLink({
+    className: 'fogbugz',
+    description: description,
+    projectName: projectElem && projectElem.textContent
+  });
+
+  $('#bugviewContainer .ixBug').prependChild(link);
+});
